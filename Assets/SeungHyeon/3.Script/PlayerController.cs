15a4a3ec94742122f2a25bce091d0070fe83ed10
JGoSeungHyeon <rhtmdgus4190@naver.com>
2023-10-26 17:28:15 +0900

    [update] Player Controller Update
    -플레이어 키보드와 컨트롤러 동시 연동 제작
    [in-progress]

--- Assets/SeungHyeon/3.Script/PlayerController.cs
+++ Assets/SeungHyeon/3.Script/PlayerController.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -6,14 +7,68 @@ using UnityEngine.InputSystem;
 public class PlayerController : MonoBehaviour
 {
     private Vector3 moveDirection;
-    [SerializeField] private float moveSpeed = 4f;
+    public float defaultSpeed = 4f;
+    [SerializeField] public float currentSpeed;
+    private Animator player_anim;
+
+    public InputAction move;
+    public InputAction run;
+
+    private void OnEnable()
+    {
+        move.performed += OnMovePerformed;
+        run.performed += OnRunPerformed;
+        run.canceled += OnRunCanceled;
+
+        move.Enable();
+        run.Enable();
+    }
+
+    private void OnDestroy()
+    {
+        move.Disable();
+        run.Disable();
+
+        move.performed -= OnMovePerformed;
+        run.performed -= OnRunPerformed;
+        run.canceled -= OnRunCanceled;
+    }
+
+    private void OnMovePerformed(InputAction.CallbackContext context)
+    {
+        var v = context.ReadValue<Vector2>();
+        moveDirection = new Vector3(v.x, 0f, v.y);
+    }
+
+    private void OnRunPerformed(InputAction.CallbackContext context)
+    {
+        var isRun = context.ReadValueAsButton();
+        if (isRun)
+            currentSpeed = defaultSpeed * 2f;
+    }
+
+    private void OnRunCanceled(InputAction.CallbackContext context)
+    {
+        currentSpeed = defaultSpeed;
+    }
+
+    private void Awake()
+    {
+        TryGetComponent(out player_anim);
+        currentSpeed = defaultSpeed;
+    }
     private void Update()
     {
         bool hasControl = (moveDirection != Vector3.zero);
         if(hasControl)
         {
             transform.rotation = Quaternion.LookRotation(moveDirection);
-            transform.Translate(Vector3.forward * moveSpeed * Time.deltaTime);
+            transform.Translate(Vector3.forward * currentSpeed * Time.deltaTime);
+            player_anim.SetInteger("AnimationPar", 1);
+        }
+        else
+        {
+            player_anim.SetInteger("AnimationPar", 0);
         }
     }
     private void OnMove(InputValue value)
@@ -22,7 +77,13 @@ public class PlayerController : MonoBehaviour
         if(input != null)
         {
             moveDirection = new Vector3(input.x, 0f, input.y);
-            Debug.Log($"Sand_MESSAGE : {input.magnitude}");
         }
+    }
+    private void OnRun(InputValue value)
+    {
+        if (value.isPressed)
+            currentSpeed = defaultSpeed * 2f;
+        else
+            currentSpeed = defaultSpeed;
     }
 }
