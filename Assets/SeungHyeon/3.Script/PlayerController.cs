f6837aba6c213646d30e556993b8c6f5f50753c5
JGoSeungHyeon <rhtmdgus4190@naver.com>
2023-10-26 16:05:01 +0900

    [update]update player move
    -플레이어 움직임 스크립트 구현
    [done]

--- Assets/SeungHyeon/3.Script/PlayerController.cs
+++ Assets/SeungHyeon/3.Script/PlayerController.cs
@@ -1,18 +1,28 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class PlayerController : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    private Vector3 moveDirection;
+    [SerializeField] private float moveSpeed = 4f;
+    private void Update()
     {
-        
+        bool hasControl = (moveDirection != Vector3.zero);
+        if(hasControl)
+        {
+            transform.rotation = Quaternion.LookRotation(moveDirection);
+            transform.Translate(Vector3.forward * moveSpeed * Time.deltaTime);
+        }
     }
-
-    // Update is called once per frame
-    void Update()
+    private void OnMove(InputValue value)
     {
-        
+        Vector2 input = value.Get<Vector2>();
+        if(input != null)
+        {
+            moveDirection = new Vector3(input.x, 0f, input.y);
+            Debug.Log($"Sand_MESSAGE : {input.magnitude}");
+        }
     }
 }
