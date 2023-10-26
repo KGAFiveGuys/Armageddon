28a5096090be7e6291cbae83daee3498f42cb470
Woo-ChanHyeok <148195213+Woo-ChanHyeok@users.noreply.github.com>
2023-10-26 15:23:17 +0900

    [update] Meteor_Movement
    - Meteor_Movement.cs : AddForce 사용해 낙하, wave에 따라 속도 변화
    [done]

--- Assets/ChanHyeok/3.Script/Meteor/Meteor_Movement.cs
+++ Assets/ChanHyeok/3.Script/Meteor/Meteor_Movement.cs
@@ -4,15 +4,34 @@ using UnityEngine;
 
 public class Meteor_Movement : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    private Rigidbody meteor_r;
+    [SerializeField] private float FallingSpeed;
+    [SerializeField] private int Wave = 1; //나중에 게임매니저에서 받아오기
+    private void Start()
     {
-        
+        meteor_r = GetComponent<Rigidbody>();
+        SettingSpeed();
     }
 
-    // Update is called once per frame
-    void Update()
+    private void FixedUpdate()
     {
-        
+        Falling();
+    }
+
+    private void Falling()
+    {
+        meteor_r.AddForce(Vector3.down * FallingSpeed * Time.deltaTime);
+    }
+
+    private void SettingSpeed()
+    {
+        if (name.Equals("Meteor_Defalut"))
+        {
+            FallingSpeed = 150f + (50 * Wave);
+        }
+        else if (name.Equals("Meteor_Dead")|| name.Equals("Meteor_Slide")|| name.Equals("Meteor_Slow"))
+        {
+            FallingSpeed = 100f;
+        }
     }
 }
