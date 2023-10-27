3bdd3afeb5833ed997ccf4c0eb6b3e8619e4a382
SangWoo <swp1207@gmail.com>
2023-10-27 15:59:28 +0900

    [create] Create tilespawner
    타일스포너 완성
    끝
    [done]

--- Assets/SangWoo/3.Script/TileBatch.cs
+++ Assets/SangWoo/3.Script/TileBatch.cs
@@ -2,17 +2,39 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+[System.Serializable]
+public class ObjectInfo
+{
+    public GameObject TilePrefabs;
+    public int Xcount;
+    public int Zcount;
+}
+
 public class TileBatch : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    [SerializeField] private ObjectInfo[] TileInfo = null;
+    public Queue<GameObject> tileQueue = new Queue<GameObject>();
+
+    private void Start()
     {
-        
+        tileQueue = InsertQueue(TileInfo[0]);
     }
 
-    // Update is called once per frame
-    void Update()
+    private Queue<GameObject> InsertQueue(ObjectInfo info)
     {
-        
+        Queue<GameObject> queue = new Queue<GameObject>();
+        for (int x = 0; x < info.Xcount; x++)
+        {
+            for (int z = 0; z < info.Xcount; z++)
+            {
+                GameObject tileClone = Instantiate(info.TilePrefabs, transform.position, Quaternion.identity);
+                tileClone.SetActive(true);
+                tileClone.transform.position = new Vector3(-9 + 6 * x, 0, 9 - 6 * z);
+                queue.Enqueue(tileClone);
+            }
+        }
+        return queue;
     }
+
+
 }
